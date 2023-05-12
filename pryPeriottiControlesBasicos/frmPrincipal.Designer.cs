d7b9ace7f39b7eb92c64173a317db2ee780d4159
Alumnos <Alumnos@PC178>
2023-05-12 13:47:37 -0300

    Agregar archivos de proyecto.

---
+++ pryPeriottiControlesBasicos/frmPrincipal.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace pryPeriottiControlesBasicos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEjeX = new System.Windows.Forms.Label();
+            this.lblEjeY = new System.Windows.Forms.Label();
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEjeX
+            // 
+            this.lblEjeX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeX.AutoSize = true;
+            this.lblEjeX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeX.Location = new System.Drawing.Point(469, 11);
+            this.lblEjeX.Name = "lblEjeX";
+            this.lblEjeX.Size = new System.Drawing.Size(25, 24);
+            this.lblEjeX.TabIndex = 0;
+            this.lblEjeX.Text = "X";
+            // 
+            // lblEjeY
+            // 
+            this.lblEjeY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeY.AutoSize = true;
+            this.lblEjeY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeY.Location = new System.Drawing.Point(471, 35);
+            this.lblEjeY.Name = "lblEjeY";
+            this.lblEjeY.Size = new System.Drawing.Size(23, 24);
+            this.lblEjeY.TabIndex = 1;
+            this.lblEjeY.Text = "Y";
+            this.lblEjeY.Click += new System.EventHandler(this.lblEjeY_Click);
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.Location = new System.Drawing.Point(12, 165);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(457, 73);
+            this.lblBienvenido.TabIndex = 2;
+            this.lblBienvenido.Text = "BIENVENIDO!";
+            this.lblBienvenido.Visible = false;
+            this.lblBienvenido.MouseEnter += new System.EventHandler(this.lblBienvenido_MouseEnter);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 450);
+            this.Controls.Add(this.lblBienvenido);
+            this.Controls.Add(this.lblEjeY);
+            this.Controls.Add(this.lblEjeX);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.Click += new System.EventHandler(this.frmPrincipal_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmPrincipal_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEjeX;
+        private System.Windows.Forms.Label lblEjeY;
+        private System.Windows.Forms.Label lblBienvenido;
+    }
+}
---
+++ pryPeriottiControlesBasicos/frmPrincipal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPeriottiControlesBasicos
+{
+    public partial class frmPrincipal : Form
+    {
+        Random random = new Random();
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPrincipal_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEjeX.Text = e.X.ToString();
+            lblEjeY.Text = e.Y.ToString();
+        }
+
+        private void lblEjeY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPrincipal_Click(object sender, EventArgs e)
+        {
+            lblBienvenido.Visible = true;
+        }
+
+        private void lblBienvenido_MouseEnter(object sender, EventArgs e)
+        {
+            lblBienvenido.Location = new Point(random.Next(10, 1000), random.Next(10, 700));
+        }
+    }
+}
